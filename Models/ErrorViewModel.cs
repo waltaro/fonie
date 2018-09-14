17c3ca6993934066f4805234a1899ef0e25fa739
Walter Ngo <ngo.walter@gmail.com>
2018-09-14 15:36:13 +1000

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fonie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
